0a64d05bbd400fe64f1e4bfc1d832ed68d225ed3
Tanimoto Yuichi <chemi@Tanimoto-no-Mac-mini.local>
2013-11-23 23:54:39 +0900

    Fix bug caused in orMerge methot

--- src/Taat.cpp
+++ src/Taat.cpp
@@ -8,14 +8,14 @@ vector<int> Taat::andMerge(const vector<int> &list1, const vector<int> &list2) {
 
   while (elem1 < list1.size() && elem2 < list2.size()) {
     if (list1[elem1] == list2[elem2]) {
-	    andResult.push_back(list1[elem1]);
-	    ++elem1;
-	    ++elem2;
-	  } else if (list1[elem1] > list2[elem2]) {
-	    ++elem2;
-	  } else {
-	    ++elem1;
-	  }
+	  andResult.push_back(list1[elem1]);
+	  ++elem1;
+	  ++elem2;
+	} else if (list1[elem1] > list2[elem2]) {
+	  ++elem2;
+	} else {
+	  ++elem1;
+	}
   }
 
   return andResult;
@@ -27,25 +27,27 @@ vector<int> Taat::orMerge(const vector<int> &list1, const vector<int> &list2) {
 
   while (elem1 < list1.size() || elem2 < list2.size()) {
     //どちらかのリストが終端まで来ている場合の処理
-	  if (elem1 >= list1.size() && elem2 < list2.size()) {
-	    orResult.push_back(list2[elem2]);
-	    ++elem2;
-	  } else if (elem1 < list1.size() && elem2 >= list2.size()) {
-	    orResult.push_back(list1[elem1]);
-	    ++elem1;
-	  }
+	if (elem1 >= list1.size() && elem2 < list2.size()) {
+	  orResult.push_back(list2[elem2]);
+	  ++elem2;
+	  continue;
+	} else if (elem1 < list1.size() && elem2 >= list2.size()) {
+	  orResult.push_back(list1[elem1]);
+	  ++elem1;
+	  continue;
+	}
 
-	  if (list1[elem1] == list2[elem2]) {
-	    orResult.push_back(list1[elem1]);
-	    ++elem1;
-	    ++elem2;
-	  } else if (list1[elem1] > list2[elem2]) {
-	    orResult.push_back(list2[elem2]);
-	    ++elem2;
-	  } else {
-	    orResult.push_back(list1[elem1]);
-	    ++elem1;
-	  }
+	if (list1[elem1] == list2[elem2]) {
+	  orResult.push_back(list1[elem1]);
+	  ++elem1;
+	  ++elem2;
+	} else if (list1[elem1] > list2[elem2]) {
+	  orResult.push_back(list2[elem2]);
+	  ++elem2;
+	} else {
+	  orResult.push_back(list1[elem1]);
+	  ++elem1;
+	}
   }
 
   return orResult;
