fd1e14131acb381c225ec1bdd079a11d6882d1da
chemi123 <chemiec@gmail.com>
2014-01-03 15:17:28 +0900

    Add Daat class

---
+++ src/Daat.cpp
@@ -0,0 +1,23 @@
+#include "Daat.hpp"
+
+using namespace std;
+
+bool isAndMergeFinished();
+bool isOrMergeFinished();
+
+vector<int> Daat::andMerge(vector< vector<int> >& vec_list) {
+  vector<int> andResult;
+	int listPtr[vec_list.size()] = {}; //全部0で初期化
+
+	while (!isAndMergeFinished()) {
+	}
+}
+
+vector<int> Daat::orMerge(vector< vector<int> >& vec_list) {
+}
+
+bool isAndMergeFinished() {
+}
+
+bool isOrMergeFinished() {
+}
---
+++ src/Daat.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+class Daat {
+public:
+  std::vector<int> andMerge(std::vector< std::vector<int> >& vec_list);
+	std::vector<int> orMerge(std::vector< std::vector<int> >& vec_list);
+};
