0467a8d3abce6229a255d2ae5430f683214da83e
Tanimoto Yuichi <chemi@Tanimoto-no-Mac-mini.local>
2014-02-21 02:49:25 +0900

    Shit

--- unitFractionPartition.cpp
+++ unitFractionPartition.cpp
@@ -10,22 +10,28 @@ int num_pattern = 0;
 //denominator:枝分かれした時点で最小の分母
 //product:総積
 
-void calc(double &value, int a, int &n, double &denominator, double product) {
+void calc(double value, int a, int &n, double &denominator, double product) {
   if (--n < 0) return;
   
   for (double i = denominator; i*product <= a; ++i) {
-    value = value - (1/i);
+    double offset = value - (1/i);
 
-	if (fabs(value) < 0.000001) {
+	if (fabs(offset) < 0.00000000000001) {
 	  ++num_pattern;
 	  continue;
+	}
+
+    if (offset > 0) {
+	  calc(offset, a, n, i, i*product);
 	}
   }
 }
 
 void solve(int p, int q, int a, int n) {
-  double value = p / q;
-  calc(value, a, n, 2, 1);
+  double init_denominator = 2;
+  double product = 1;
+  double value = (double)p/q;
+  calc(value, a, n, init_denominator, product);
 
   cout << num_pattern << endl;
 }
@@ -36,10 +42,9 @@ int main() {
   while (true) {
     int p, q, a, n;
 	fin >> p >> q >> a >> n;
-	if (n == 0) break;
+	if (p == 0 && q == 0 & a == 0 && n == 0) break;
+    solve(p, q, a, n);
   }
-
-  solve(p, q, a, n);
   
   return 0;
 }
